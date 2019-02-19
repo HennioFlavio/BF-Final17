ef6ec7372778869d46e715d53451b2b81d0262e0
h2n100@gmail.com <h2n100@gmail.com>
2019-02-19 00:15:44 -0300

    Projeto 04 – Etapa 08

--- app/src/main/java/com/example/bollyfilmes/FilmesAdapter.java
+++ app/src/main/java/com/example/bollyfilmes/FilmesAdapter.java
@@ -11,6 +11,9 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class FilmesAdapter extends ArrayAdapter<ItemFilme> {
+    private static final int VIEW_TYPE_DESTAQUE = 0;
+
+    private static final int VIEW_TYPE_ITEM = 1;
 
     public FilmesAdapter(Context context, ArrayList<ItemFilme> filmes) {
         super(context, 0, filmes);
@@ -19,26 +22,58 @@ public class FilmesAdapter extends ArrayAdapter<ItemFilme> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        View itemView = convertView;
-        if (itemView == null){
-            itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_filme, parent, false);
-
-        }
-
+        int viewType = getItemViewType(position);
         ItemFilme filme = getItem(position);
+        View itemView = convertView;
 
-        TextView titulo = (TextView) itemView.findViewById(R.id.item_titulo);
-        titulo.setText(filme.getTitulo());
 
-        TextView desc = (TextView) itemView.findViewById(R.id.item_desc);
-        desc.setText(filme.getDescricao());
 
-        TextView dataLancamento = (TextView) itemView.findViewById(R.id.item_data);
-        dataLancamento.setText(filme.getDataLancamento());
+        switch (viewType) {
+            case VIEW_TYPE_DESTAQUE:{
 
-        RatingBar avaliacao = (RatingBar) itemView.findViewById(R.id.item_avaliacao);
-        avaliacao.setRating(filme.getAvaliacao());
+                itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_filme_destaque, parent, false);
 
+
+                TextView desc = (TextView) itemView.findViewById(R.id.item_desc);
+                desc.setText(filme.getDescricao());
+
+                RatingBar avaliacao = (RatingBar) itemView.findViewById(R.id.item_avaliacao);
+                avaliacao.setRating(filme.getAvaliacao());
+                break;
+
+
+            }
+
+            case VIEW_TYPE_ITEM:{
+
+                itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_filme, parent, false);
+
+                TextView titulo = (TextView) itemView.findViewById(R.id.item_titulo);
+                titulo.setText(filme.getTitulo());
+
+                TextView desc = (TextView) itemView.findViewById(R.id.item_desc);
+                desc.setText(filme.getDescricao());
+
+                TextView dataLancamento = (TextView) itemView.findViewById(R.id.item_data);
+                dataLancamento.setText(filme.getDataLancamento());
+
+                RatingBar avaliacao = (RatingBar) itemView.findViewById(R.id.item_avaliacao);
+                avaliacao.setRating(filme.getAvaliacao());
+                break;
+            }
+        }
         return itemView;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (position == 0 ? VIEW_TYPE_DESTAQUE : VIEW_TYPE_ITEM);
+    }
+
+    @Override
+    public int getViewTypeCount() {
+
+        // pq sao duas views
+        return 2;
     }
 }
