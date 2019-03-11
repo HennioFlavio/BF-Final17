f6ae43c9f44f1b668817e3e3098f5a979cbb2b64
h2n100@gmail.com <h2n100@gmail.com>
2019-03-11 00:31:49 -0300

    Projeto 04 – Etapa 11

--- app/src/main/java/com/example/bollyfilmes/MainActivity.java
+++ app/src/main/java/com/example/bollyfilmes/MainActivity.java
@@ -21,49 +21,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ListView list = (ListView) findViewById(R.id.list_filmes);
-
-        final ArrayList<ItemFilme> arrayList = new ArrayList<>();
-        arrayList.add(new ItemFilme("Homem Aranha", "Filme de heroi picado por uma aranha", "10/04/2016", 4));
-        arrayList.add(new ItemFilme("Homem Cobra", "Filme de heroi picado por uma cobra", "06/01/2016", 2));
-        arrayList.add(new ItemFilme("Homem Javali", "Filme de heroi levou chifrada de um javali", "30/06/2016", 3));
-        arrayList.add(new ItemFilme("Homem Passaro", "Filme de heroi bicado por um passaro", "23/05/2016", 5));
-        arrayList.add(new ItemFilme("Homem Cachorro", "Filme de heroi mordido por um cachorro", "14/02/2016", 3.5f));
-        arrayList.add(new ItemFilme("Homem Gato", "Filme de heroi arranhado por um gato", "10/04/2016", 2.5f));
-
-        FilmesAdapter adapter = new FilmesAdapter(this, arrayList);
-        list.setAdapter(adapter);
-
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-                ItemFilme itemFilme = arrayList.get(position);
-                Intent intent = new Intent(MainActivity.this, FilmeDetalheActivity.class);
-                intent.putExtra(KEY_FILME, itemFilme);
-                startActivity(intent);
-            }
-        });
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.menu_atualizar:
-                Toast.makeText(this, "Atualizando os filmes...", Toast.LENGTH_LONG).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-
-
     }
 }
---
+++ app/src/main/java/com/example/bollyfilmes/MainFragment.java
@@ -0,0 +1,83 @@
+package com.example.bollyfilmes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // acrecentou esse inflate tirou do return e colocou no return somente a view
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // colou ess codigo do MainActivity
+        ListView list = (ListView) view.findViewById(R.id.list_filmes);
+
+        final ArrayList<ItemFilme> arrayList = new ArrayList<>();
+        arrayList.add(new ItemFilme("Homem Aranha", "Filme de heroi picado por uma aranha", "10/04/2016", 4));
+        arrayList.add(new ItemFilme("Homem Cobra", "Filme de heroi picado por uma cobra", "06/01/2016", 2));
+        arrayList.add(new ItemFilme("Homem Javali", "Filme de heroi levou chifrada de um javali", "30/06/2016", 3));
+        arrayList.add(new ItemFilme("Homem Passaro", "Filme de heroi bicado por um passaro", "23/05/2016", 5));
+        arrayList.add(new ItemFilme("Homem Cachorro", "Filme de heroi mordido por um cachorro", "14/02/2016", 3.5f));
+        arrayList.add(new ItemFilme("Homem Gato", "Filme de heroi arranhado por um gato", "10/04/2016", 2.5f));
+
+        FilmesAdapter adapter = new FilmesAdapter(getContext(), arrayList);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                ItemFilme itemFilme = arrayList.get(position);
+                Intent intent = new Intent(getContext(), FilmeDetalheActivity.class);
+                intent.putExtra(MainActivity.KEY_FILME, itemFilme);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_atualizar:
+                Toast.makeText(getContext(), "Atualizando os filmes...", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+}
